6d8c1612a0fe33731ee8afd83cf083d71b5e2413
zl20072008zl <zl20072008zl@gmail.com>
2017-11-29 14:18:14 -0500

    Add files via upload

---
+++ FingerPaintPolyline.cs
@@ -0,0 +1,18 @@
+﻿using Android.Graphics;
+
+namespace MultiTouch
+{
+    public class FingerPaintPolyline
+    {
+        public FingerPaintPolyline()
+        {
+            Path = new Path();
+        }
+
+        public Color Color { set; get; }
+
+        public float StrokeWidth { set; get; }
+
+        public Path Path { private set; get; }
+    }
+}
